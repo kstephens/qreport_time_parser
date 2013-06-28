3cd00adb15bc64dc416e47f75c59dc980003f7be
Kurt Stephens <ks.github@kurtstephens.com>
2013-06-28 12:21:20 -0500

    More test cases.

--- spec/lib/qreport/time_parser_spec.rb
+++ spec/lib/qreport/time_parser_spec.rb
@@ -8,10 +8,15 @@ describe Qreport::TimeParser do
     examples.each do | expr, val |
       next if Symbol === expr
       it "should translate #{expr.inspect} to #{val.inspect}" do
-        tp = Qreport::TimeParser.new
-        tp.now = now
-        # tp.debug = true if expr =~ /between/i
-        t = tp.parse(expr)
+        t = nil
+        begin
+          tp = Qreport::TimeParser.new
+          tp.now = now
+          # tp.debug = true if expr =~ /between/i
+          t = tp.parse(expr)
+        rescue Qreport::TimeParser::Error => exc
+          t = exc
+        end
         t.to_s.should == val
       end
     end
