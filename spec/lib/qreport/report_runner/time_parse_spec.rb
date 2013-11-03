349fd94110d36f48265938a3a441252d19d0eabf
Kurt Stephens <ks.github@kurtstephens.com>
2013-11-03 01:28:18 -0500

    Better pending desc.

--- spec/lib/qreport/report_runner/time_parse_spec.rb
+++ spec/lib/qreport/report_runner/time_parse_spec.rb
@@ -23,12 +23,13 @@ describe Qreport::ReportRunner::TimeParse do
       subject.time_parse("now").should == (::Time.parse("2011-04-27T08:23:37.981304") ... ::Time.parse("2011-04-27T08:23:38.981304")) # questionable?
     end
     it "should parse 'now' with unit_for_now[:now] = :now." do
-      pending
+      pending "incorrect default unit creates 1-sec range"
       p.unit_for_now[:now] = :now
       subject.time_parse("now").should == ::Time.parse("2011-04-27T08:23:37.981304")
     end
     it "should parse 't' with unit_for_now[:t] = :now." do
-      pending
+      pending "incorrect default unit creates 1-sec range"
+      p.debug = 1
       subject.time_parse("t - 10 sec").should == ::Time.parse("2011-04-27T08:23:37.981304")
     end
     it "should parse relative date." do
